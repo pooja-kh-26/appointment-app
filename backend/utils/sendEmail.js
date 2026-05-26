3f61ae1e98be8e38aa43a5db2fa253e82e070d88
pooja-kh-26 <poojakh2026@gmail.com>
2026-05-26 08:21:05 +0530

    Fixed email sending and added Jitsi meeting integration

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,26 +1,60 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.MEETING_EMAIL,
         pass: process.env.MEETING_EMAIL_PASSWORD,
+    },
+    tls: {
+        rejectUnauthorized: false,
     },
 });
 
 const sendEmail = async (to, subject, html) => {
     try {
-        await transporter.sendMail({
+        const info = await transporter.sendMail({
             from: process.env.MEETING_EMAIL,
             to,
             subject,
             html,
         });
 
-        console.log("Email sent to:", to);
+        console.log("Email sent:", info.response);
     } catch (error) {
         console.error("Email error:", error);
     }
 };
 
 module.exports = sendEmail;
+
+
+
+// const nodemailer = require("nodemailer");
+
+// const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//         user: process.env.MEETING_EMAIL,
+//         pass: process.env.MEETING_EMAIL_PASSWORD,
+//     },
+// });
+
+// const sendEmail = async (to, subject, html) => {
+//     try {
+//         await transporter.sendMail({
+//             from: process.env.MEETING_EMAIL,
+//             to,
+//             subject,
+//             html,
+//         });
+
+//         console.log("Email sent to:", to);
+//     } catch (error) {
+//         console.error("Email error:", error);
+//     }
+// };
+
+// module.exports = sendEmail;
